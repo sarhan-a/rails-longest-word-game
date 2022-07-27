daf8a2c5e20104f1690d44a8602896f49f23134b
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-28 00:04:29 +0100

    complete controller and actions. views functional

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,29 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   VOWELS = %w(A E I O U Y)
   def new
-    @letters = Array.new(3) { VOWELS.sample }
-    @letters += Array.new(7) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(4) { VOWELS.sample }
+    @letters += Array.new(6) { ('A'..'Z').to_a.sample }
     @letters.shuffle!
   end
 
   def score
-    raise
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end

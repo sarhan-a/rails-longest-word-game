460a95df662965ab8c8ee7339b292bd68db08dc3
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-27 01:26:02 +0100

    progress on controller actions and views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,12 @@
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
   def new
+    @letters = Array.new(3) { VOWELS.sample }
+    @letters += Array.new(7) { ('A'..'Z').to_a.sample }
+    @letters.shuffle!
   end
 
   def score
+    raise
   end
 end
